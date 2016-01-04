3e090b0f37d40e5e0e9fc98b0a9e29f4fa5c6804
Vinay Lakkam <vinay.lakkam@gmail.com>
2016-01-04 11:49:40 +0530

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name posAdminApp
+ * @description
+ * # posAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('posAdminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name posAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the posAdminApp
+ */
+angular.module('posAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
